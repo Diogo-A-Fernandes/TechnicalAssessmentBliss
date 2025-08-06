ff16a9e78606b0ab9ca05d28b08b3b2cd7720748
Diogo Fernandes <diogoperas86@gmail.com>
2025-08-06 18:04:46 +0200

    Commit inicial

---
+++ BlissAppTests/BlissAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  BlissAppTests.swift
+//  BlissAppTests
+//
+//  Created by user284528 on 8/6/25.
+//
+
+import Testing
+@testable import BlissApp
+
+struct BlissAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
