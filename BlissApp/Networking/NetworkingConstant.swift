f4c5f76a3ad6b980be5c9dce098cd9f137dc3985
Diogo Fernandes <diogoperas86@gmail.com>
2025-08-08 23:29:07 +0200

    feat: add network constants for API endpoints

---
+++ BlissApp/Networking/NetworkingConstant.swift
@@ -0,0 +1,32 @@
+//
+//  NetworkingConstant.swift
+//  BlissApp
+//
+//  Created by Diogo on 8/8/25.
+//
+
+import UIKit
+
+class NetworkingConstant{
+    private init(){
+        
+    }
+    
+    public var emojiApi: String{
+        get{
+            return "https://api.github.com/emojis"
+        }
+    }
+    
+    public var usersApi: String{
+        get{
+            return "https://api.github.com/emojis"
+        }
+    }
+    
+    public var repositoriesApi: String{
+        get{
+            return "https://api.github.com/emojis"
+        }
+    }
+}
