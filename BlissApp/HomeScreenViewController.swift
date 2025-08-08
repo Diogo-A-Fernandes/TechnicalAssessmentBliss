4f31347a0615b21d493f88ea0da936125209f18f
Diogo Fernandes <diogoperas86@gmail.com>
2025-08-08 17:39:03 +0200

    chore: add test label

--- BlissApp/HomeScreenViewController.swift
+++ BlissApp/HomeScreenViewController.swift
@@ -9,8 +9,29 @@ import UIKit
 
 class HomeScreenViewController: UIViewController{
     
+    
     override func viewDidLoad(){
         super.viewDidLoad()
+        view.backgroundColor = .white
+        self.title = "Main Screen"
+        configurarLabel()
     }
+    
+    private func configurarLabel() {
+        let label = UILabel()
+        label.text = "Hello"
+        label.textColor = .black
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(label)
+        
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+            label.widthAnchor.constraint(equalToConstant: 200),
+            label.heightAnchor.constraint(equalToConstant: 40)
+        ])
+    }
+
     
 }
