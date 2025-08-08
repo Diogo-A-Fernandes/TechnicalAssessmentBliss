48bf3968338693c933b0041f61cf792798be987d
Diogo Fernandes <diogoperas86@gmail.com>
2025-08-09 00:33:20 +0200

    refactor: make variables accessible at class scope

--- BlissApp/HomeScreen/HomeScreenView.swift
+++ BlissApp/HomeScreen/HomeScreenView.swift
@@ -9,68 +9,72 @@ import UIKit
 
 class HomeScreenView: UIView {
     
-    
+    let randomEmojiImageView = UIImageView()
+    let randomEmojiButton = UIButton()
+    let emojisListButton = UIButton()
+    let searchBar = UISearchBar()
+    let searchButton = UIButton()
+    let avatarsListButton = UIButton()
+    let repositoriesButton = UIButton()
+
     override init(frame: CGRect) {
         super.init(frame: frame)
-        setUpVerticalStack()
+        configureSubviews()
+        buildVerticalStack()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
-        setUpVerticalStack()
+        configureSubviews()
+        buildVerticalStack()
     }
     
-    private func setUpVerticalStack() {
-        
-        let homeScreenStack = UIStackView(arrangedSubviews: setupSubviewsForVerticalStack())
-        homeScreenStack.axis = .vertical
-        homeScreenStack.spacing = 8
-        homeScreenStack.translatesAutoresizingMaskIntoConstraints = false
-        self.addSubview(homeScreenStack)
-        
-        
-        NSLayoutConstraint.activate([
-            homeScreenStack.centerXAnchor.constraint(equalTo: self.centerXAnchor),
-            homeScreenStack.centerYAnchor.constraint(equalTo: self.centerYAnchor),
-            homeScreenStack.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.9)
-        ])
-    }
-    
-    private func setupSubviewsForVerticalStack() -> [UIView]{
-        
-        let randomEmojiImageView = UIImageView()
+    private func configureSubviews() {
         randomEmojiImageView.backgroundColor = .gray
         randomEmojiImageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
         
-        let randomEmojiButton = UIButton()
         randomEmojiButton.backgroundColor = .gray
         randomEmojiButton.setTitle("Random Emoji", for: .normal)
         
-        let emojisListButton = UIButton()
         emojisListButton.backgroundColor = .gray
         emojisListButton.setTitle("Emojis List", for: .normal)
         
-        let searchBar = UISearchBar()
         searchBar.placeholder = "Search"
         searchBar.searchBarStyle = .minimal
         
-        let searchButton = UIButton()
         searchButton.backgroundColor = .gray
         searchButton.setTitle("Search", for: .normal)
         
-        let horizontalStack = UIStackView(arrangedSubviews: [searchBar
-                                                             ,searchButton])
-        horizontalStack.axis = .horizontal
-        
-        let avatarsListButton = UIButton()
         avatarsListButton.backgroundColor = .gray
         avatarsListButton.setTitle("Avatars List", for: .normal)
         
-        let repositoriesButton = UIButton()
         repositoriesButton.backgroundColor = .gray
         repositoriesButton.setTitle("Apple Repos", for: .normal)
-        
-        return [randomEmojiImageView,randomEmojiButton,emojisListButton,horizontalStack,avatarsListButton,repositoriesButton]
     }
     
+    private func buildVerticalStack() {
+        let horizontalStack = UIStackView(arrangedSubviews: [searchBar, searchButton])
+        horizontalStack.axis = .horizontal
+        horizontalStack.spacing = 8
+        
+        let homeScreenStack = UIStackView(arrangedSubviews: [
+            randomEmojiImageView,
+            randomEmojiButton,
+            emojisListButton,
+            horizontalStack,
+            avatarsListButton,
+            repositoriesButton
+        ])
+        homeScreenStack.axis = .vertical
+        homeScreenStack.spacing = 8
+        homeScreenStack.translatesAutoresizingMaskIntoConstraints = false
+        
+        addSubview(homeScreenStack)
+        
+        NSLayoutConstraint.activate([
+            homeScreenStack.centerXAnchor.constraint(equalTo: centerXAnchor),
+            homeScreenStack.centerYAnchor.constraint(equalTo: centerYAnchor),
+            homeScreenStack.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.9)
+        ])
+    }
 }
