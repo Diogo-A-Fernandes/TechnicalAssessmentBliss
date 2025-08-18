8aa98f40a2c3a83d7a88dd5e18e111b4011d8fad
Diogo Fernandes <diogoperas86@gmail.com>
2025-08-18 14:42:05 +0100

    feat: create view using reusable collection view component

--- BlissApp/AvatarsScreen/AvatarsView.swift
+++ BlissApp/AvatarsScreen/AvatarsView.swift
@@ -5,3 +5,30 @@
 //  Created by Diogo on 8/16/25.
 //
 
+import UIKit
+
+class AvatarsView: UIView {
+    
+    var avatarsCollectionView : GridCollectionView = GridCollectionView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    private func setUpLayout(){
+        
+        addSubview(avatarsCollectionView)
+        
+        NSLayoutConstraint.activate([
+            avatarsCollectionView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor),
+            avatarsCollectionView.leftAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leftAnchor,constant: 10),
+            avatarsCollectionView.rightAnchor.constraint(equalTo: self.safeAreaLayoutGuide.rightAnchor,constant: -10),
+            avatarsCollectionView.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+}
