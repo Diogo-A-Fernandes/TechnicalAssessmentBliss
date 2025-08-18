a7018dc90e96de76d05e1bffae50067d2d7db6dd
Diogo Fernandes <diogoperas86@gmail.com>
2025-08-18 14:44:51 +0100

    feat: create view controller as a bridge between home screen and avatars screen

--- BlissApp/AvatarsScreen/AvatarsViewController.swift
+++ BlissApp/AvatarsScreen/AvatarsViewController.swift
@@ -5,3 +5,42 @@
 //  Created by Diogo on 8/16/25.
 //
 
+import UIKit
+
+class AvatarsViewController: UIViewController {
+    
+    let viewModel: AvatarsViewModel
+    let avatarsView = AvatarsView()
+    
+    init(viewModel: AvatarsViewModel = AvatarsViewModel()) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        self.viewModel = AvatarsViewModel()
+        super.init(coder: coder)
+    }
+    
+    override func loadView() {
+        self.view = avatarsView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        title = "Avatars"
+        setupCollectionView()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        fetchAvatarsAndReload()
+    }
+    
+    private func fetchAvatarsAndReload() {
+        viewModel.fetchAvatars { [weak self] _ in
+            self?.reloadDataCollectionView()
+        }
+    }
+}
