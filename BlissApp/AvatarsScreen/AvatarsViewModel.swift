8146454b99c43d35e518ab174d08b138e410f566
Diogo Fernandes <diogoperas86@gmail.com>
2025-08-18 14:46:03 +0100

    feat: connect view model with repository and API

--- BlissApp/AvatarsScreen/AvatarsViewModel.swift
+++ BlissApp/AvatarsScreen/AvatarsViewModel.swift
@@ -5,3 +5,31 @@
 //  Created by Diogo on 8/16/25.
 //
 
+class AvatarsViewModel {
+    
+    private let _avatarRepository: AvatarsRepository = AvatarsDataRepository()
+    private var avatars: [Avatars] = []
+    
+    func fetchAvatars(completionHandler: @escaping ([Avatars]) -> Void) {
+        avatars = _avatarRepository.getAll()
+        completionHandler(avatars)
+    }
+    
+    func numberOfItems() -> Int {
+        return avatars.count
+    }
+    
+    func getAvatars() -> [Avatars] {
+        return avatars
+    }
+    
+    func getAvatar(username: String) -> Avatars? {
+        return _avatarRepository.getAvatar(username: username)
+    }
+    
+    func deleteAvatars(avatar: Avatars, index: Int) -> Bool {
+        guard index < avatars.count else { return false }
+        avatars.remove(at: index)
+        return _avatarRepository.delete(record: avatar)
+    }
+}
