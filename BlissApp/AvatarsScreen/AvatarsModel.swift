f9d9edc40349fad1ddc30dfe87d9b08375dd94d3
Diogo Fernandes <diogoperas86@gmail.com>
2025-08-19 21:17:16 +0100

    refactor: update model to decode only needed JSON fields to reduce unnecessary data

--- BlissApp/AvatarsScreen/AvatarsModel.swift
+++ BlissApp/AvatarsScreen/AvatarsModel.swift
@@ -5,69 +5,13 @@
 //  Created by Diogo on 8/16/25.
 //
 
-import Foundation
-
 // MARK: - AvatarsModel
 struct AvatarsModel: Codable {
-    let login: String?
-    let id: Int?
-    let nodeID: String?
-    let avatarURL: String?
-    let gravatarID: String?
-    let url: String?
-    let htmlURL: String?
-    let followersURL: String?
-    let followingURL: String?
-    let gistsURL: String?
-    let starredURL: String?
-    let subscriptionsURL: String?
-    let organizationsURL: String?
-    let reposURL: String?
-    let eventsURL: String?
-    let receivedEventsURL: String?
-    let type: String?
-    let userViewType: String?
-    let siteAdmin: Bool?
-    let name: String?
-    let company: String?
-    let blog: String?
-    let location: String?
-    let email: String?
-    let hireable: String?
-    let bio: String?
-    let twitterUsername: String?
-    let publicRepos: Int?
-    let publicGists: Int?
-    let followers: Int?
-    let following: Int?
-    let createdAt: Date?
-    let updatedAt: Date?
+    let login: String
+    let avatarURL: String
 
     enum CodingKeys: String, CodingKey {
-        case login, id
-        case nodeID = "node_id"
+        case login
         case avatarURL = "avatar_url"
-        case gravatarID = "gravatar_id"
-        case url
-        case htmlURL = "html_url"
-        case followersURL = "followers_url"
-        case followingURL = "following_url"
-        case gistsURL = "gists_url"
-        case starredURL = "starred_url"
-        case subscriptionsURL = "subscriptions_url"
-        case organizationsURL = "organizations_url"
-        case reposURL = "repos_url"
-        case eventsURL = "events_url"
-        case receivedEventsURL = "received_events_url"
-        case type
-        case userViewType = "user_view_type"
-        case siteAdmin = "site_admin"
-        case name, company, blog, location, email, hireable, bio
-        case twitterUsername = "twitter_username"
-        case publicRepos = "public_repos"
-        case publicGists = "public_gists"
-        case followers, following
-        case createdAt = "created_at"
-        case updatedAt = "updated_at"
     }
 }
