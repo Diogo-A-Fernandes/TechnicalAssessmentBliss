1b1f0e52f5c4212a4ccfe8a3012647343356889a
Diogo Fernandes <diogoperas86@gmail.com>
2025-08-22 00:22:02 +0100

    refactor: make all API calls uniform

--- BlissApp/Networking/APICaller.swift
+++ BlissApp/Networking/APICaller.swift
@@ -7,35 +7,45 @@
 
 import UIKit
 
-enum NetworkError: Error{
+enum NetworkError: Error {
     case urlError
     case canNotParseData
 }
 
-public class APICaller{
+public class APICaller {
     
-    static func getEmojies(completionHandler: @escaping(_ result: Result<EmojisModel,NetworkError>) -> Void){
+    private static let itemsSize = 10
+    private static var pageNumber = 1
+    
+    
+    // MARK: - Emojis
+    static func getEmojies(completionHandler: @escaping (_ result: Result<EmojisModel, NetworkError>) -> Void) {
         
         let urlString = NetworkingConstant.shared.emojiApi
         
-        guard let url = URL(string: urlString) else{
+        guard let url = URL(string: urlString) else {
             completionHandler(.failure(.urlError))
             return
         }
         
-        URLSession.shared.dataTask(with: url){dataResponse, urlResponse, error in
-            if error == nil,
-               let data = dataResponse,
-               let result = try? JSONDecoder().decode(EmojisModel.self,from: data){
-                completionHandler(.success(result))
+        URLSession.shared.dataTask(with: url) { dataResponse, _, error in
+            guard error == nil, let data = dataResponse else {
+                completionHandler(.failure(.canNotParseData))
+                return
             }
-            else{
+            
+            do {
+                let result = try JSONDecoder().decode(EmojisModel.self, from: data)
+                completionHandler(.success(result))
+            } catch {
+                print("Decoding error (Emojis):", error)
                 completionHandler(.failure(.canNotParseData))
             }
         }.resume()
     }
     
-    static func getAvatar(username: String, completionHandler: @escaping(_ result: Result<AvatarsModel,NetworkError>) -> Void) {
+    // MARK: - Avatar
+    static func getAvatar(username: String, completionHandler: @escaping (_ result: Result<AvatarsModel, NetworkError>) -> Void) {
         
         let urlString = NetworkingConstant.shared.usersApi + username
         
@@ -44,56 +54,46 @@ public class APICaller{
             return
         }
         
-        print(url)
-        URLSession.shared.dataTask(with: url) { dataResponse, urlResponse, error in
-            
+        URLSession.shared.dataTask(with: url) { dataResponse, _, error in
             guard error == nil, let data = dataResponse else {
                 completionHandler(.failure(.canNotParseData))
                 return
             }
             
-            let decoder = JSONDecoder()
-            decoder.dateDecodingStrategy = .iso8601
-            
             do {
-                let result = try decoder.decode(AvatarsModel.self, from: data)
+                let result = try JSONDecoder().decode(AvatarsModel.self, from: data)
                 completionHandler(.success(result))
             } catch {
-                print("Decoding error:", error)
+                print("Decoding error (Avatar):", error)
                 completionHandler(.failure(.canNotParseData))
             }
-            
         }.resume()
     }
     
+    // MARK: - Apple Repos
     static func getAppleRepos(completionHandler: @escaping (_ result: Result<[RepositoriesModel], NetworkError>) -> Void) {
         
-        let urlString = NetworkingConstant.shared.repositoriesApi + "page=1&per_page=10"
+        let urlString = NetworkingConstant.shared.repositoriesApi + "page=\(pageNumber)&per_page=\(itemsSize)"
         
         guard let url = URL(string: urlString) else {
             completionHandler(.failure(.urlError))
             return
         }
         
-        print(url)
-        URLSession.shared.dataTask(with: url) { dataResponse, urlResponse, error in
-            
+        URLSession.shared.dataTask(with: url) { dataResponse, _, error in
             guard error == nil, let data = dataResponse else {
                 completionHandler(.failure(.canNotParseData))
                 return
             }
             
-            let decoder = JSONDecoder()
-            decoder.dateDecodingStrategy = .iso8601
-            
             do {
-                let result = try decoder.decode([RepositoriesModel].self, from: data)
+                let result = try JSONDecoder().decode([RepositoriesModel].self, from: data)
+                pageNumber += 1
                 completionHandler(.success(result))
             } catch {
-                print("Decoding error:", error)
+                print("Decoding error (Repositories):", error)
                 completionHandler(.failure(.canNotParseData))
             }
-            
         }.resume()
     }
 }
